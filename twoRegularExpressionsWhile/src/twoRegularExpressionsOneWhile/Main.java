4bb11b470ac8bc3cc5f8020ca4b46c5cec1f463f
Lio Bouchan <liogothicvampire@gmail.com>
2018-12-02 21:14:44 -0600

    2 matchers in 1 while

--- twoRegularExpressionsWhile/src/twoRegularExpressionsOneWhile/Main.java
+++ twoRegularExpressionsWhile/src/twoRegularExpressionsOneWhile/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
 		String parrafo = "(53) Anexo 32 \r\n" + //
 				"Anexo 3 \r\n" + //
 				"(53) Anexo 23. \r\n" + //
@@ -19,44 +18,33 @@ public class Main {
 
 		Pattern pattern = Pattern.compile("(Anexo )(\\p{Digit}*\\p{Digit})");
 		Matcher matcher = pattern.matcher(parrafo);
-		
+
 		Pattern pattern2 = Pattern.compile("(Anexos )(\\p{Digit}*\\p{Digit})( y )(\\p{Digit}*\\p{Digit})");
 		Matcher matcher2 = pattern2.matcher(parrafo);
-
+		
 		int apuntador = 0;
 
-		while (matcher.find(apuntador)) {
-			System.out.println("Apuntador 1: " + apuntador );
+		while (matcher.find(apuntador) || matcher2.find(apuntador)) {
+			System.out.println("Apuntador1 1: " + apuntador );
 			System.out.println("Start "+ matcher.start());
 			System.out.println("End: "+ matcher.end());
 			apuntador = matcher.end();
 			System.out.println("Anexo: " + matcher.group());
-			//System.out.println("Contenido:"+ parrafo.substring(matcher.end()).trim());
 			System.out.println(" Numero de Grupos " + matcher.groupCount());
-			System.out.println(" GRUPO 1 " + matcher.group(0));
-			System.out.println(" GRUPO 2 " + matcher.group(1));
-			System.out.println(" GRUPO 2 " + matcher.group(2));
-			System.out.println(" Apuntador 2: " + apuntador );
-		}
-
-		
-
-		int apuntador2 = 0;
-
-		while (matcher2.find(apuntador2)) {
-			System.out.println("Apuntador 1: " + apuntador2 );
-			System.out.println("Start "+ matcher2.start());
-			System.out.println("End: "+ matcher2.end());
-			apuntador2 = matcher2.end();
-			System.out.println("Anexos: " + matcher2.group());
-			//System.out.println("Contenido:"+ parrafo.substring(matcher.end()).trim());
-			System.out.println("Numero de Grupos " + matcher2.groupCount());
-			System.out.println("GRUPO 1 " + matcher2.group(0));
-			System.out.println("GRUPO 2 " + matcher2.group(1));
-			System.out.println("GRUPO 2 " + matcher2.group(2));
-			System.out.println("Apuntador 2: " + apuntador2 );
+			
+			if (matcher.groupCount() == 2) {
+				System.out.println(" GRUPO 1 " + matcher.group(0));
+				System.out.println(" GRUPO 2 " + matcher.group(1));
+				System.out.println(" GRUPO 3 " + matcher.group(2));
+			}
+			if (matcher.groupCount() == 4) {
+				System.out.println(" GRUPO 1 " + matcher2.group(0));
+				System.out.println(" GRUPO 2 " + matcher2.group(1));
+				System.out.println(" GRUPO 3 " + matcher2.group(2));
+				System.out.println(" GRUPO 4 " + matcher2.group(3));
+				System.out.println(" GRUPO 5 " + matcher2.group(4));
+			}
+			System.out.println(" Apuntador1 2: " + apuntador );
 		}
 	}
-	}
-
 }
