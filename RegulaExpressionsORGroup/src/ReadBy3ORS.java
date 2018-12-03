ffd80054e32ee8bf92bdcdd137f0a2190bf3bbbe
Leonardo <liogothicvampire@gmail.com>
2018-12-03 13:25:42 -0600

    Fail the first 3 OR in one sentence

---
+++ RegulaExpressionsORGroup/src/ReadBy3ORS.java
@@ -0,0 +1,40 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReadBy3ORS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		String parrafo = "(53) Anexo 32 \r\n" + //
+				"Anexo 3 \r\n" + //
+				"Anexo 1-A \r\n" + //
+				"(53) Anexo 1-B \r\n" + //
+				"(53) Anexo 23. \r\n" + //
+				"(53) Anexo 2. \r\n" + //
+				"(53) Anexos 2 y 5 \r\n" + //
+				"(53) Anexos 22 y 34 \r\n" + //
+				"(53) Anexo 3434. Cuenta 7.\r\n" + //
+				"(53) Anexo 3 , Cuenta 8.";
+
+		Pattern pattern = Pattern.compile("(Anexo )(\\p{Digit}*\\p{Digit}\\-\\p{Upper}|\\p{Digit}*\\p{Digit})");
+		Matcher matcher = pattern.matcher(parrafo);
+
+		int apuntador = 0;
+
+		while (matcher.find(apuntador)) {
+			System.out.println("Apuntador 1: " + apuntador );
+			System.out.println("Start "+ matcher.start());
+			System.out.println("End: "+ matcher.end());
+			apuntador = matcher.end();
+			System.out.println("Anexo: " + matcher.group());
+			//System.out.println("Contenido:"+ parrafo.substring(matcher.end()).trim());
+			System.out.println(" Numero de Grupos " + matcher.groupCount());
+			System.out.println(" GRUPO 1 " + matcher.group(0));
+			System.out.println(" GRUPO 2 " + matcher.group(1));
+			System.out.println(" GRUPO 2 " + matcher.group(2));
+			System.out.println(" Apuntador 2: " + apuntador );
+		}
+	}
+
+}
