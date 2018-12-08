33589634f9c1b1e134643637c6a716426a89a806
Leonardo <liogothicvampire@gmail.com>
2018-12-07 18:57:06 -0600

    Need to be implemented the example code are on regularExpressions/Main.java

--- RegulaExpressionsORGroup/src/ReadBy3ORsAndIfWithMoreRegexs.java
+++ RegulaExpressionsORGroup/src/ReadBy3ORsAndIfWithMoreRegexs.java
@@ -1,8 +1,40 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ReadBy3ORsAndIfWithMoreRegexs {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		String parrafo = "(53) Anexo 32 \r\n" + //
+				"Anexo 3 \r\n" + //
+				"Anexo 1-A \r\n" + //
+				"(53) Anexo 1-B \r\n" + //
+				"(53) Anexo 1-C Bis. \r\n" + //
+				"(53) Anexo 1-D Bis 1. \r\n" + //
+				"(53) Anexo 23. \r\n" + //
+				"(53) Anexo 2. \r\n" + //
+				"(53) Anexos 2 y 5 \r\n" + //
+				"(53) Anexos 22 y 34 \r\n" + //
+				"(53) Anexo 3434. Cuenta 7.\r\n" + //
+				"(53) Anexo 3 , Cuenta 8.";
+
+		Pattern pattern = Pattern.compile("(Anexo )((\\p{Digit}*\\p{Digit}\\-[A-Z])( Bis)( \\p{Digit})*|\\p{Digit}*\\p{Digit}\\-\\p{Upper}|\\p{Digit}*\\p{Digit})");
+		Pattern pattern2 = Pattern.compile("(Anexos )(\\p{Digit}*\\p{Digit})( y )(\\p{Digit}*\\p{Digit})");
+
+		int apuntador = 0;
+
+		while (matcher.find(apuntador)) {
+			System.out.println("Apuntador 1: " + apuntador );
+			System.out.println("Start "+ matcher.start());
+			System.out.println("End: "+ matcher.end());
+			System.out.println("Anexo: " + matcher.group());
+			
+			for (int i = 0; i <= matcher.groupCount(); i++) {
+				System.out.println(" Grupo " + i + " " + matcher.group(i));
+			}
+			
+			apuntador = matcher.end();
+			System.out.println("Apuntador 2: " + apuntador );
+		}
 
 	}
 
