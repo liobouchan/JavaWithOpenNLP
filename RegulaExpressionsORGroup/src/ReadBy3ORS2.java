b2ae4660657c9c08dacf6b051507d278278fed81
Leonardo <liogothicvampire@gmail.com>
2018-12-11 14:26:17 -0600

    Pattern ready of regular expression

--- RegulaExpressionsORGroup/src/ReadBy3ORS2.java
+++ RegulaExpressionsORGroup/src/ReadBy3ORS2.java
@@ -6,20 +6,22 @@ public class ReadBy3ORS2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
-		String parrafo = "(53) Anexo 32 \r\n" + //
-				"Anexo 3 \r\n" + //
-				"Anexo 1-A \r\n" + //
-				"(53) Anexo 1-B \r\n" + //
-				"(53) Anexo 1-C Bis. \r\n" + //
-				"(53) Anexo 1-D Bis 1. \r\n" + //
-				"(53) Anexo 23. \r\n" + //
-				"(53) Anexo 2. \r\n" + //
-				"(53) Anexos 2 y 5 \r\n" + //
-				"(53) Anexos 22 y 34 \r\n" + //
-				"(53) Anexo 3434. Cuenta 7.\r\n" + //
-				"(53) Anexo 3 , Cuenta 8.";
+		String parrafo = "(53) Articulo 1 \r\n" + //
+				"Artículo 2 \r\n" + //
+				"Artículo 22 \r\n" + //
+				"Artículo 1 Bis \r\n" + //
+				"Artículo 2 Bis 1 \r\n" + //
+				"Artículo 3 Bis 22 \r\n" + //
+				"Artículo 4 Bis 22 a \r\n" + //
+				"Artículo 5 Bis 2 a \r\n" + //
+				"Artículo 66 Bis \r\n" + //
+				"Artículo 77 Bis 1 \r\n" + //
+				"Artículo 88 Bis 22 \r\n" + //
+				"Artículo 99 Bis 33 a \r\n" + //
+				"Artículo 11 Bis 4 a \r\n" + //
+				"Artículo 22 Bis 2 z \r\n";
 
-		Pattern pattern = Pattern.compile("(Anexo )((\\p{Digit}*\\p{Digit}\\-[A-Z])( Bis)( \\p{Digit})*|\\p{Digit}*\\p{Digit}\\-\\p{Upper}|\\p{Digit}*\\p{Digit})");
+		Pattern pattern = Pattern.compile("(Artículo )(\\p{Digit}*\\p{Digit})( Bis )*(\\p{Digit}*\\p{Digit})*( \\p{Lower})*");
 		Matcher matcher = pattern.matcher(parrafo);
 
 		int apuntador = 0;
