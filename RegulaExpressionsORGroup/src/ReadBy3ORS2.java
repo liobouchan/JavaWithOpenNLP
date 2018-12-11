b6dedfc588e4b1997a8fdca4f33252cbb41e7e43
Leonardo <liogothicvampire@gmail.com>
2018-12-11 13:18:39 -0600

    Create a new class to probe with Articles

---
+++ RegulaExpressionsORGroup/src/ReadBy3ORS2.java
@@ -0,0 +1,42 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReadBy3ORS2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		String parrafo = "(53) Anexo 32 \r\n" + //
+				"Anexo 3 \r\n" + //
+				"Anexo 1-A \r\n" + //
+				"(53) Anexo 1-B \r\n" + //
+				"(53) Anexo 1-C Bis. \r\n" + //
+				"(53) Anexo 1-D Bis 1. \r\n" + //
+				"(53) Anexo 23. \r\n" + //
+				"(53) Anexo 2. \r\n" + //
+				"(53) Anexos 2 y 5 \r\n" + //
+				"(53) Anexos 22 y 34 \r\n" + //
+				"(53) Anexo 3434. Cuenta 7.\r\n" + //
+				"(53) Anexo 3 , Cuenta 8.";
+
+		Pattern pattern = Pattern.compile("(Anexo )((\\p{Digit}*\\p{Digit}\\-[A-Z])( Bis)( \\p{Digit})*|\\p{Digit}*\\p{Digit}\\-\\p{Upper}|\\p{Digit}*\\p{Digit})");
+		Matcher matcher = pattern.matcher(parrafo);
+
+		int apuntador = 0;
+
+		while (matcher.find(apuntador)) {
+			System.out.println("Apuntador 1: " + apuntador );
+			System.out.println("Start "+ matcher.start());
+			System.out.println("End: "+ matcher.end());
+			System.out.println("Anexo: " + matcher.group());
+			
+			for (int i = 0; i <= matcher.groupCount(); i++) {
+				System.out.println(" Grupo " + i + " " + matcher.group(i));
+			}
+			
+			apuntador = matcher.end();
+			System.out.println("Apuntador 2: " + apuntador );
+		}
+	}
+
+}
