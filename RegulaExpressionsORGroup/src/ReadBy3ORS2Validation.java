7672ffa3f9383c10954f7b74fa297e85b191d2b7
Leonardo <liogothicvampire@gmail.com>
2018-12-14 17:54:02 -0600

    Created the validation Algortim

--- RegulaExpressionsORGroup/src/ReadBy3ORS2Validation.java
+++ RegulaExpressionsORGroup/src/ReadBy3ORS2Validation.java
@@ -1,11 +1,12 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ReadBy3ORS2 {
+public class ReadBy3ORS2Validation {
 
+	private final static String LINE_IDENTIFIER = "\\r?\\n";
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
+
 		String parrafo = "(53) Articulo 1 \r\n" + //
 				"Artículo 2 \r\n" + //
 				"Artículo 22 \r\n" + //
@@ -21,34 +22,59 @@ public class ReadBy3ORS2 {
 				"Artículo 99 Bis 33 y \r\n" + //
 				"Artículo 11 Bis 4 a \r\n" + //
 				"Artículo 11 Bis 4 o Bla bla bla basura \r\n" + //
+				"Artículo 11 Bis 4 o Anexo 34-p \r\n" + //
 				"Artículo 22 Bis 2 z \r\n";
 
 		Pattern pattern = Pattern.compile("(Artículo )(\\p{Digit}*\\p{Digit})( Bis )*(\\p{Digit}*\\p{Digit})*( [a-x] )*");
-		Matcher matcher = pattern.matcher(parrafo);
+		String lines[] = parrafo.split(LINE_IDENTIFIER);
 
 		//Pattern pattern2 = Pattern.compile("(Artículo )(\\p{Digit}*\\p{Digit})( Bis )*(\\p{Digit}*\\p{Digit})*( o )");
-		
-		
-		int apuntador = 0;
 
-		while (matcher.find()) {
-			//System.out.println("Apuntador 1: " + apuntador );
-			//System.out.println("Start "+ matcher.start());
-			//System.out.println("End: "+ matcher.end());
-			System.out.println("Artículo: " + matcher.group());
-			//String contenido = parrafo.substring(matcher.end());
-			//System.out.println("Contenido: " + contenido );
-			//String articulito = matcher.group();
-			//Matcher matcher2 = pattern2.matcher(articulito);
-			//if (matcher2.find()) {
-			//	System.out.println("	Este es el que trae el pedo");
-			//}
-			//for (int i = 0; i <= matcher.groupCount(); i++) {
-			//	System.out.println(" Grupo " + i + " " + matcher.group(i));
-			//}
-			
-			//apuntador = matcher.end();
-			//System.out.println("Apuntador 2: " + apuntador );
+		int apuntador = 0;
+		
+		for (int i = 0; i < lines.length; i++) {
+			//System.out.println("Linea: " + lines[i]);
+			Matcher matcher = pattern.matcher(lines[i]);
+			//int apuntador = 0;
+ 			if (matcher.find()) {
+				//System.out.println("Apuntador 1: " + apuntador );
+				//System.out.println("Start "+ matcher.start());
+				//System.out.println("End: "+ matcher.end());
+				//apuntador = matcher.end();
+ 				String articulo = matcher.group();
+ 				String contenido = lines[i].substring(matcher.end()).trim();
+				System.out.println("Anexo: " + matcher.group());
+				System.out.println("Contenido: "+ contenido);
+				if (articulo.contains(" o ")) {
+					System.out.println("sI TIENE UNA 0");
+					if (contenido.contains("Bis")|| contenido.contains("Anexo")) {
+						System.out.println("Esto es un o de verdad");
+					}else {
+						System.out.println("Entonces si tiene inciso");
+					}
+				}
+				//System.out.println("Apuntador 2: " + apuntador );
+			}
 		}
+
+//		while (matcher.find()) {
+//			//System.out.println("Apuntador 1: " + apuntador );
+//			//System.out.println("Start "+ matcher.start());
+//			//System.out.println("End: "+ matcher.end());
+//			System.out.println("Artículo: " + matcher.group());
+//			//String contenido = parrafo.substring(matcher.end());
+//			//System.out.println("Contenido: " + contenido );
+//			//String articulito = matcher.group();
+//			//Matcher matcher2 = pattern2.matcher(articulito);
+//			//if (matcher2.find()) {
+//			//	System.out.println("	Este es el que trae el pedo");
+//			//}
+//			//for (int i = 0; i <= matcher.groupCount(); i++) {
+//			//	System.out.println(" Grupo " + i + " " + matcher.group(i));
+//			//}
+//			
+//			//apuntador = matcher.end();
+//			//System.out.println("Apuntador 2: " + apuntador );
+//		}
 	}
 }
